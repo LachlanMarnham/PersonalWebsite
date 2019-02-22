9d4774095615b6fccbc855c3e9a8d2f9cd7bbb2f
LachlanMarnham <lachlan.marnham@gmail.com>
2019-02-22 02:20:40 +0000

    Add run_apache.sh to fix container restart issue

---
+++ server_config/run_apache.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Make sure we're not confused by old, incompletely-shutdown httpd
+# context after restarting the container.  httpd won't start correctly
+# if it thinks it is already running.
+rm -rf /run/httpd/* /tmp/httpd*
+
+echo "Booting apache..."
+
+exec /usr/sbin/httpd -D FOREGROUND
