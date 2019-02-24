57cd07c10b9dac1c038b8097958c1a009dafb68b
LachlanMarnham <lachlan.marnham@gmail.com>
2019-02-24 03:54:23 +0000

    Added youtube videos

--- app/helpers.py
+++ app/helpers.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from collections import namedtuple
 from enum import Enum, unique
 from flask import url_for
@@ -42,7 +44,7 @@ class NavBarMenu:
             context=NavContext.cv,
         )
 
-    def items(self):
+    def items(self) -> Tuple[MenuItem, MenuItem, MenuItem, MenuItem, MenuItem]:
         return (
             self.home,
             self.music,
@@ -54,10 +56,40 @@ class NavBarMenu:
 
 # The main menu has only a subset of the items that the nav bar has
 class MainMenu(NavBarMenu):
-    def items(self):
+    def items(self) -> Tuple[MenuItem, MenuItem, MenuItem, MenuItem]:
         return (
             self.music,
             self.projects,
             self.partita,
             self.cv
+        )
+
+
+class MusicVideos:
+    frame_border = 0
+    allowed_options = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+
+    def __init__(self):
+        self._claire_de_lune = "https://www.youtube.com/embed/NhCcd-7DxAk?wmode=transparent"
+        self._se_ela_perguntar = "https://www.youtube.com/embed/cTjFNhBM-pw"
+        self._rumores_de_la_caleta = "https://www.youtube.com/embed/adUsXe464ow"
+        self._recuerdos_de_la_alhambra = "https://www.youtube.com/embed/x6OhZz1qHMI"
+        self._caprico_arabe = "https://www.youtube.com/embed/PpdDOV3NmCM"
+        self._asturias = "https://www.youtube.com/embed/wfb5orlwtHE"
+        self._nocturne = "https://www.youtube.com/embed/MqQdqJ7osYQ"
+        self._prelude = "https://www.youtube.com/embed/Cb3uU9kUPgo"
+        self._registro = "https://www.youtube.com/embed/3qPen_gpJX4"
+
+    @property
+    def urls(self) -> Tuple[str, str, str, str, str, str, str, str, str]:
+        return (
+            self._claire_de_lune,
+            self._rumores_de_la_caleta,
+            self._se_ela_perguntar,
+            self._asturias,
+            self._nocturne,
+            self._caprico_arabe,
+            self._recuerdos_de_la_alhambra,
+            self._prelude,
+            self._registro,
         )
--- app/templates/music.html.mako
+++ app/templates/music.html.mako
@@ -1,3 +1,18 @@
 <%inherit file="content_base.html.mako" />
 
-<div class="jumbotron"><h1>Under construction...</h1></div>
+<%!
+    from app.helpers import MusicVideos
+    music_videos = MusicVideos()
+%>
+
+%for video_url in music_videos.urls:
+    <div style="position:relative; width:100%; height:0px; padding-bottom:56.25%;">
+        <iframe style="position:absolute; left:0; top:0; width:100%; height:100%"
+            src="${video_url}"
+            frameborder="${music_videos.frame_border}"
+            allow="${music_videos.allowed_options}"
+            allowfullscreen>
+        </iframe>
+    </div>
+    <br />
+%endfor
